b5b0c6b6ce58d708024d911be1f13b414fdba92d
Akshat <akshatsingh99@gmail.com>
2019-05-23 19:40:18 +1000

    Google Sign In Tests

--- app/src/main/java/com/dude/funky/charitylookup/MainActivity.java
+++ app/src/main/java/com/dude/funky/charitylookup/MainActivity.java
@@ -1,13 +1,155 @@
 package com.dude.funky.charitylookup;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.squareup.picasso.Picasso;
+
+import junit.framework.Test;
 
 public class MainActivity extends AppCompatActivity {
+
+    static final int GOOGLE_SIGN = 123;
+    FirebaseAuth mAuth;
+    Button btn_login,btn_logout;
+    TextView text;
+    ImageView image;
+    ProgressBar progressBar;
+    GoogleSignInClient mGoogleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_login = findViewById(R.id.login);
+        btn_logout = findViewById(R.id.logout);
+        text = findViewById(R.id.text);
+        image = findViewById(R.id.image);
+        progressBar = findViewById(R.id.progress_circular);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions
+                .Builder()
+                //.requestIdToken(getString(R.string.default_web_client_id))
+                .requestIdToken("254819476708-on9hda2bpc7r5bq6687khteopnnhh849.apps.googleusercontent.com")
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this,googleSignInOptions);
+
+        btn_login.setOnClickListener(v->SignInGoogle());
+        btn_logout.setOnClickListener(v->Logout());
+
+        if(mAuth.getCurrentUser() != null){
+            FirebaseUser user = mAuth.getCurrentUser();
+        }
     }
+
+    void SignInGoogle() {
+        progressBar.setVisibility(View.VISIBLE);
+        Intent signIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signIntent, GOOGLE_SIGN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == GOOGLE_SIGN);
+            Task<GoogleSignInAccount> task = GoogleSignIn
+                    .getSignedInAccountFromIntent(data);
+
+            try {
+
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                if (account!=null) firebaseAuthWithGoogle(account);
+                    //make request firebase
+
+            }   catch (ApiException e){
+                e.printStackTrace();
+            }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount account) {
+
+        Log.d("TAG","firebaseAuthWithGoogle: " + account.getId());
+
+        AuthCredential credential = GoogleAuthProvider
+                .getCredential(account.getIdToken(),null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, task -> {
+                    if(task.isSuccessful()){
+                        progressBar.setVisibility(View.INVISIBLE);
+                        Log.d("TAG","signin success");
+
+                        Toast.makeText(this,"SignIn", Toast.LENGTH_SHORT);
+
+                        FirebaseUser user = mAuth.getCurrentUser();
+
+                    } else {
+                        progressBar.setVisibility(View.INVISIBLE);
+                        Log.w("TAG","signin failure", task.getException());
+
+                        Toast.makeText(this,"SignIn Failed!", Toast.LENGTH_SHORT);
+                        updateUI(null);
+                    }
+                });
+    }
+
+    private void updateUI(FirebaseUser user) {
+        if(user != null) {
+
+            String name = user.getDisplayName();
+            String email = user.getEmail();
+            String photo = String.valueOf(user.getPhotoUrl());
+
+            text.append("Info: \n");
+            text.append(name + "\n");
+            text.append(email);
+
+
+            Picasso.get().load(photo).into(image);
+            btn_login.setVisibility(View.INVISIBLE);
+            btn_logout.setVisibility(View.VISIBLE);
+
+        }   else {
+
+            text.setText(getString(R.string.firebase_login));
+            Picasso.get().load(R.drawable.ic_firebase_logo);
+            btn_login.setVisibility(View.VISIBLE);
+            btn_logout.setVisibility(View.INVISIBLE);
+
+
+        }
+    }
+
+    void Logout() {
+
+        FirebaseAuth.getInstance().signOut();
+        mGoogleSignInClient.signOut()
+                .addOnCompleteListener(this, task -> {
+                   updateUI(null);
+                });
+
+    }
+
 }
---
+++ app/src/main/java/com/dude/funky/charitylookup/Pass.java
@@ -0,0 +1,66 @@
+package com.dude.funky.charitylookup;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.ybs.passwordstrengthmeter.PasswordStrength;
+
+public class Pass extends AppCompatActivity implements TextWatcher {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pass);
+
+        EditText password = (EditText)findViewById(R.id.login_password);
+        password.addTextChangedListener(this);
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+    }
+    @Override
+    public void beforeTextChanged(
+            CharSequence s, int start, int count, int after) {
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+        updatePasswordStrengthView(s.toString());
+    }
+
+    private void updatePasswordStrengthView(String password) {
+
+        ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        TextView strengthView = (TextView) findViewById(R.id.password_strength);
+        if (TextView.VISIBLE != strengthView.getVisibility())
+            return;
+
+        if (password.isEmpty()) {
+            strengthView.setText("");
+            progressBar.setProgress(0);
+            return;
+        }
+
+        PasswordStrength str = PasswordStrength.calculateStrength(password);
+        strengthView.setText(str.getText(this));
+        strengthView.setTextColor(str.getColor());
+
+        progressBar.getProgressDrawable().setColorFilter(str.getColor(), android.graphics.PorterDuff.Mode.SRC_IN);
+        if (str.getText(this).equals("Weak")) {
+            progressBar.setProgress(25);
+        } else if (str.getText(this).equals("Medium")) {
+            progressBar.setProgress(50);
+        } else if (str.getText(this).equals("Strong")) {
+            progressBar.setProgress(75);
+        } else {
+            progressBar.setProgress(100);
+        }
+    }
+}
