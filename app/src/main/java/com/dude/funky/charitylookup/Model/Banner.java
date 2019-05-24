2b0991293bd03026d3e860e7634c3df453fdb163
Akshat <akshatsingh99@gmail.com>
2019-05-24 16:21:18 +1000

    HomePage Init

---
+++ app/src/main/java/com/dude/funky/charitylookup/Model/Banner.java
@@ -0,0 +1,21 @@
+package com.dude.funky.charitylookup.Model;
+
+public class Banner {
+
+    private String image;
+
+    public Banner(String image) {
+        this.image = image;
+    }
+
+    public Banner() {
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
