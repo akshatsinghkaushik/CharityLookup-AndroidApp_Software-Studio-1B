86837eb548bad45019f67c1124404b6493f69825
jasminadang <jasminadang@gmail.com>
2019-05-26 20:16:35 +1000

    Merge branch 'Jaffar_Registration_XML' of https://github.com/akshatsinghkaushik/CharityLookup into Jaffar_Registration_XML
    # Conflicts:
    #	app/src/main/res/values/strings.xml

---
+++ app/src/main/java/com/dude/funky/charitylookup/DonorProfile.java
@@ -0,0 +1,13 @@
+package com.dude.funky.charitylookup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DonorProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_donor_profile);
+    }
+}
